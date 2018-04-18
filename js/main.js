5b7b0105d23bc9ff4b8aaf89fae73e4700fc710d
MartinKoeoep <36571063+MartinKoeoep@users.noreply.github.com>
2018-04-18 10:07:00 +0100

    created game logic
    Added functions

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 var cards = ["queen" , "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log(cardsInPlay);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log(cardsInPlay);
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again!")
+function checkForMatch() {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+		} else { console.log("Sorry, try again!") }
+		}
 	}
-}
+function flipCard(cardId) {
+	console.log(cards[cardId] + " was flipped over.");
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+	}
+
+flipCard(0);
