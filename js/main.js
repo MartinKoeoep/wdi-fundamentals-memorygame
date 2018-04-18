66e9482f7bd3dd734d1397b471dbebc6f1119bee
MartinKoeoep <36571063+MartinKoeoep@users.noreply.github.com>
2018-04-18 09:40:50 +0100

    Added game logic

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen" , "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log(cardsInPlay);
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log(cardsInPlay);
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again!")
+	}
+}
